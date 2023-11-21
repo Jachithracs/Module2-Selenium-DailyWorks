f1861d9ad37b0e43d8dbf0cde9e4441a098b0f0a
Jachithracs <jachithra.cs@ust.com>
2023-11-21 20:01:59 +0530

    Assignment-4_21-11-2023 Committed

--- Assignment_4-21-11-2023/NaaptolTest.cs
+++ Assignment_4-21-11-2023/NaaptolTest.cs
@@ -84,10 +84,31 @@ namespace Assignment_4_21_11_2023
             buyProduct.Click();
             Thread.Sleep(3000);
 
-
-
-
         }
+        [Test]
+        [Order(40)]
+        [Author("Jachithra", "cs@gmail.com")]
+        [Description("View Cart product")]
+        [Category("Regression Testing")]
+
+        public void ViewProduct()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(10);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(100);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fluentWait.Message = "Elemet Not Found";
+
+            IWebElement viewProduct = wait.Until(driv => driv.FindElement(By.LinkText("Reading Glasses with LED Lights (LRG4)")));
+            Assert.AreEqual(viewProduct.Text, "Reading Glasses with LED Lights (LRG4)");
+            Console.WriteLine("Product is already in the cart");
+            Thread.Sleep(3000);
+
+            IWebElement closePopup = wait.Until(driv => driv.FindElement(By.XPath("//*[contains(@class,'fancybox-close')]")));
+            closePopup.Click();
+        }
+
 
     }
 }
